ae03b028796f3eac8bc09fd1ed650316f702268c
Кальченко Тимофей <hackerbro228007@mail.ru>
2025-02-16 19:53:58 +0300

    fix all and YA ZAEBALSYA

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,38 +1,44 @@
-from main import bot, dp
 from keyboards import keyboard
 from aiogram import types
-from aiogram.dispatcher.filters import Command
+from aiogram.filters import Command
+from dotenv import load_dotenv
+import os
+from aiogram import F
 
-@dp.message_handler(Command('start'))
-async def start(message: types.Message):
-    await bot.send_message(message.chat.id, 'Тестируем WebApp!',
+load_dotenv(".env")
+PAY_TOKEN = os.getenv('PAY_TOKEN')
+
+def setup_handlers(dp):
+    @dp.message(Command('start'))
+    async def start(message: types.Message):
+        await message.answer('Тестируем WebApp!',
                            reply_markup=keyboard)
 
-PRICE = {
-    '1': [types.LabeledPrice(label='Item1', amount=100000)],
-    '2': [types.LabeledPrice(label='Item2', amount=200000)],
-    '3': [types.LabeledPrice(label='Item3', amount=300000)],
-    '4': [types.LabeledPrice(label='Item4', amount=400000)],
-    '5': [types.LabeledPrice(label='Item5', amount=500000)],
-    '6': [types.LabeledPrice(label='Item6', amount=600000)]
-}
+    PRICE = {
+        '1': [types.LabeledPrice(label='Item1', amount=1)],
+        '2': [types.LabeledPrice(label='Item2', amount=2)],
+        '3': [types.LabeledPrice(label='Item3', amount=3)],
+        '4': [types.LabeledPrice(label='Item4', amount=4)],
+        '5': [types.LabeledPrice(label='Item5', amount=5)],
+        '6': [types.LabeledPrice(label='Item6', amount=6)]
+    }
 
-@dp.message_handler(content_types='web_app_data')
-async def buy_process(web_app_message):
-    await bot.send_invoice(web_app_message.chat.id,
-                           title='Laptop',
-                           description='Description',
-                           provider_token='pay_token',
-                           currency='rub',
-                           need_email=True,
-                           prices=PRICE[f'{web_app_message.web_app_data.data}'],
-                           start_parameter='example',
-                           payload='some_invoice')
+    @dp.message(F.web_app_data)
+    async def buy_process(web_app_message):
+        await web_app_message.answer_invoice(
+            title='Laptop',
+            description='Description',
+            provider_token=PAY_TOKEN,
+            currency='rub',
+            need_email=True,
+            prices=PRICE[f'{web_app_message.web_app_data.data}'],
+            start_parameter='example',
+            payload='some_invoice')
 
-@dp.pre_checkout_query_handler(lambda query: True)
-async def pre_checkout_process(pre_checkout: types.PreCheckoutQuery):
-    await bot.answer_pre_checkout_query(pre_checkout.id, ok=True)
+    @dp.pre_checkout_query
+    async def pre_checkout_process(pre_checkout: types.PreCheckoutQuery):
+        await pre_checkout.answer(ok=True)
 
-@dp.message_handler(content_types=types.ContentType.SUCCESSFUL_PAYMENT)
-async def successful_payment(message: types.Message):
-    await bot.send_message(message.chat.id, 'Платеж прошел успешно!')
+    @dp.message(lambda message: message.content_type == types.ContentType.SUCCESSFUL_PAYMENT)
+    async def successful_payment(message: types.Message):
+        await message.answer('Платеж прошел успешно!')
--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,64 @@
 from aiogram import Bot, Dispatcher
-
 import asyncio
+from dotenv import load_dotenv 
+import os
+from aiogram import types
+from aiogram.filters import Command
+from aiogram.types import WebAppInfo, LabeledPrice, ReplyKeyboardMarkup, KeyboardButton
 
-bot = Bot(token='token')
-dp = Dispatcher(bot=bot)
+load_dotenv(".env")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+PAY_TOKEN = os.getenv('PAY_TOKEN')
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+web_app = WebAppInfo(url='https://neforskiy.github.io/index.html')
+
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Сайт', web_app=web_app)]
+    ],
+    resize_keyboard=True
+)
+
+PRICE = {
+    '1': [LabeledPrice(label='Item1', amount=1)],
+    '2': [LabeledPrice(label='Item2', amount=2)],
+    '3': [LabeledPrice(label='Item3', amount=3)],
+    '4': [LabeledPrice(label='Item4', amount=4)],
+    '5': [LabeledPrice(label='Item5', amount=5)],
+    '6': [LabeledPrice(label='Item6', amount=6)]
+}
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.answer('Тестируем WebApp!', reply_markup=keyboard)
+
+@dp.message(lambda message: message.web_app_data)
+async def buy_process(web_app_message):
+    await web_app_message.answer_invoice(
+        title='Laptop',
+        description='Description',
+        provider_token=PAY_TOKEN,
+        currency='rub',
+        need_email=True,
+        prices=PRICE[f'{web_app_message.web_app_data.data}'],
+        start_parameter='example',
+        payload='some_invoice')
+
+@dp.pre_checkout_query
+async def pre_checkout_process(pre_checkout: types.PreCheckoutQuery):
+    await pre_checkout.answer(ok=True)
+
+@dp.message(lambda message: message.content_type == types.ContentType.SUCCESSFUL_PAYMENT)
+async def successful_payment(message: types.Message):
+    await message.answer('Платеж прошел успешно!')
 
 async def main():
-    from handlers import dp
-    try:
-        await dp.start_polling()
-    finally:
-        await bot.session.close()
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot, skip_updates=True)
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        print('Bot stopped!')
+    asyncio.run(main())
